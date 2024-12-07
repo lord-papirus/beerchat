dfc0e245e60b0cd51af10d63e14d1e988b81db28
lord-papirus <iotest@onenavigation.net>
2024-12-07 23:16:20 +0300

    Update plugin/priv_cc.lua
    Co-authored-by: SX <50966843+S-S-X@users.noreply.github.com>

--- plugin/priv_cc.lua
+++ plugin/priv_cc.lua
@@ -1,5 +1,5 @@
 -- if you need to set a special name for privilege, use "beerchat.name_priv_cc = spec_name" in minetest.conf
-local name_priv2cc = minetest.settings:get("beerchat.name_priv_cc")
+local name_priv2cc = minetest.settings:get("beerchat.priv_cc.name")
 if name_priv2cc == '' then name_priv2cc = 'chat_creator' end
 minetest.log("info", "to create channel add new priv: " .. name_priv2cc)
 local S = minetest.get_translator("name_priv2cc")
